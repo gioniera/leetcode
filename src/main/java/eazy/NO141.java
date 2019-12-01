e44e9e611813d419baa2952d01c5eddd33a52428
wangyl <gioniera@gmail.com>
2019-12-01 11:49:17 +0800

    141 判断环形链表

---
+++ src/main/java/eazy/NO141.java
@@ -0,0 +1,54 @@
+package eazy;
+import java.util.HashSet;
+
+/**
+ * @author: WYL
+ * @create: 2019-12-01 10:34
+ *
+ **/
+public class NO141 {
+    /**
+     * 快慢指针 一个环 慢指针每次走一步 快指针每次走两步 如果是环 快指针总能追上慢指针
+     * @param head
+     * @return
+     */
+    public boolean hasCycle1(ListNode head) {
+        if (head==null||head.next==null){
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next!=null&&fast.next.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * hash 保存head地址 重复就是环
+     * @param head
+     * @return
+     */
+    public boolean hasCycle(ListNode head) {
+        HashSet<ListNode> set = new HashSet<>();
+        while (head!=null&&head.next!=null){
+            if (!set.add(head)){
+                return true;
+            }
+            head = head.next;
+        }
+        return false;
+    }
+}
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+}
