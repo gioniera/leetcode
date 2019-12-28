44de7bc9b518fc01a9c2698b27b22dbdaad09876
wangyl <gioniera@gmail.com>
2019-12-28 22:20:51 +0800

    101. 对称二叉树

---
+++ src/main/java/eazy/NO101.java
@@ -0,0 +1,64 @@
+package eazy;
+
+import vo.TreeNode;
+
+import java.util.LinkedList;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+/**
+ * @author: WYL
+ * @create: 2019-12-28 20:47
+ * 对称二叉树
+ **/
+public class NO101 {
+    /**
+     * 递归
+     * @param root
+     * @return
+     */
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root,root);
+    }
+    public boolean isMirror(TreeNode left,TreeNode right){
+        if (left==null&&right==null){
+            return true;
+        }
+        if (left==null||right==null){
+            return false;
+        }
+        return left.val==right.val
+                &&isMirror(left.left,right.right)
+                &&isMirror(left.right,right.left);
+    }
+
+    /**
+     *
+     * @param root
+     * @return
+     */
+    public boolean isSymmetric2(TreeNode root) {
+        LinkedList<TreeNode> treeNodes = new LinkedList<>();
+        treeNodes.add(root);
+        treeNodes.add(root);
+        while (!treeNodes.isEmpty()){
+            TreeNode t1 = treeNodes.pop();
+            TreeNode t2 = treeNodes.pop();
+            if (t1==null&&t2==null){
+                continue;
+            }
+            if (t1==null||t2==null){
+                return false;
+            }
+            if (t1.val != t2.val){
+                return false;
+            }
+            treeNodes.add(t1.left);
+            treeNodes.add(t2.right);
+            treeNodes.add(t1.right);
+            treeNodes.add(t2.left);
+        }
+
+        return true;
+
+    }
+}
--- src/main/java/eazy/NO104.java
+++ src/main/java/eazy/NO104.java
@@ -1,10 +1,8 @@
 package eazy;
 
-import javafx.util.Pair;
 import vo.TreeNode;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedDeque;
 
 /**
  * @author: WYL
@@ -17,22 +15,66 @@ public class NO104 {
      * @param root
      * @return
      */
-    public int maxDepth(TreeNode root) {
+    public int maxDepth(String str, TreeNode root) {
         if (root==null){
+            System.out.println(str+"null");
             return 0;
         }else {
-            int lh = maxDepth(root.left);;
-            int rh = maxDepth(root.right);
+            System.out.println(str+"root=" + root.val + "   01");
+            int lh = maxDepth(str+"\t", root.left);
+            System.out.println(str+"root=" + root.val + "   02");
+            int rh = maxDepth(str+"\t", root.right);
+            System.out.println(str+"root=" + root.val + "   03");
             return java.lang.Math.max(lh,rh)+1;
         }
     }
 
-    /**
-     * 迭代 栈
-     * @param root
-     * @return
-     */
-    public int maxDepth2(TreeNode root) {
-        return 0;
+    public  int dfsSum(TreeNode root){
+        int sum = root.val;
+        if (root.left!=null){
+            sum += dfsSum(root.left);
+        }
+        if (root.right!=null){
+            sum += dfsSum(root.right);
+        }
+        return sum;
+    }
+
+    public int bfsSum(TreeNode root){
+        int sum = 0;
+        ConcurrentLinkedDeque<TreeNode> treeNodes = new ConcurrentLinkedDeque<>();
+        treeNodes.add(root);
+        while (!treeNodes.isEmpty()){
+            TreeNode node = treeNodes.pop();
+            sum += node.val;
+            if (node.left!=null){
+                treeNodes.add(node.left);
+            }
+            if (node.right!=null){
+                treeNodes.add(node.right);
+            }
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        TreeNode left1 = new TreeNode(2);
+        TreeNode right1 = new TreeNode(3);
+        TreeNode left2 = new TreeNode(2);
+        TreeNode right2 = new TreeNode(3);
+        TreeNode left3 = new TreeNode(2);
+        TreeNode right3 = new TreeNode(3);
+        root.left = left1;
+        root.right = right1;
+//        left1.left=left2;
+//        left1.right=right2;
+//        right1.left=left3;
+//        right1.right=right3;
+        NO104 no = new NO104();
+//        no.maxDepth("", root);
+//        no.dfsSum(root);
+        System.out.println(no.bfsSum(root));
     }
 }
